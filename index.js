a2cbfd0674cc4fdec6521ad0c4fc6fc6b6f407c5
dani <dani.zaghian@gmail.com>
2021-10-04 10:53:10 -0700

    adds webpack and require statements

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-/* global moment */
+var moment = require('moment');
+var math = require('mathjs');
+
 console.log("Hello from JavaScript!");
 var startOfHour = moment().startOf('hour').fromNow();
 console.log(startOfHour);
+
+var logResult = math.log(10000, 10);
+console.log(logResult);
